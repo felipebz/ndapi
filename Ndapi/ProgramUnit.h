e3918734440687349185a151a41e3c232d5cedc2
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2014-01-24 22:36:06 -0200

    Make ProgramUnit constructor public

--- Ndapi/ProgramUnit.h
+++ Ndapi/ProgramUnit.h
@@ -7,10 +7,10 @@ namespace Ndapi
 	public ref class ProgramUnit : NdapiObject
 	{
 	public protected:
-		ProgramUnit();
 		ProgramUnit(d2fob* program_unit);
 
 	public:
+		ProgramUnit();
 		property String^ Comment { String^ get(); void set(String^ value); }
 		property String^ Text { String^ get(); void set(String^ value); }
 	};
