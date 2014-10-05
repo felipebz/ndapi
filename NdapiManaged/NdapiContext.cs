fbf723a5826ccd57d1215c121c0ee3dd681e2634
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2014-10-05 10:29:44 -0300

    Código managed

---
+++ NdapiManaged/NdapiContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NdapiManaged
+{
+    public class NdapiContext
+    {
+        private static IntPtr _context;
+
+        public static IntPtr Context
+        {
+            get
+            {
+                if (_context == IntPtr.Zero)
+                {
+                    var context_attributes = new d2fctxa();
+                    context_attributes.mask_d2fctxa = 0;
+                    var status = NativeMethods.d2fctxcr_Create(ref _context, ref context_attributes);
+                }
+                return _context;
+            }
+        }
+
+        public static int ProductVersion
+        {
+            get
+            {
+                uint version = 0;
+                var status = NativeMethods.d2fctxbv_BuilderVersion(Context, ref version);
+                return (int)version;
+            }
+        }
+    }
+}
