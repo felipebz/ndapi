353559e1a5c4e6b2cc58978d963b9f564e164a9b
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2014-11-26 21:18:47 -0200

    Added a nuspec

--- Ndapi/Properties/AssemblyInfo.cs
+++ Ndapi/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Ndapi")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A .NET wrapper for Oracle Forms 6i Open API")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Felipe Zorzo")]
 [assembly: AssemblyProduct("Ndapi")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
