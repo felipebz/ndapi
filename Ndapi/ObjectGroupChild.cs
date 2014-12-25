29701137a1348d1e03316caff45ee9a060a18595
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2014-12-25 20:10:32 -0200

    Add ObjectGroupChild documentation (closes #26)

--- Ndapi/ObjectGroupChild.cs
+++ Ndapi/ObjectGroupChild.cs
@@ -3,8 +3,17 @@ using Ndapi.Enums;
 
 namespace Ndapi
 {
+    /// <summary>
+    /// Represents an object group child. Every object must be wrapped in an "object group child" to add it
+    /// in an object group.
+    /// </summary>
     public class ObjectGroupChild : NdapiObject
     {
+        /// <summary>
+        /// Creates an object group child.
+        /// </summary>
+        /// <param name="group">Object owner.</param>
+        /// <param name="name">Object name.</param>
         public ObjectGroupChild(ObjectGroup group, string name)
         {
             Create(name, ObjectType.ObjectGroupChild, group);
@@ -14,6 +23,9 @@ namespace Ndapi
         {
         }
 
+        /// <summary>
+        /// Gets or sets the real object.
+        /// </summary>
         public NdapiObject RealObject
         {
             get { return GetObjectProperty<NdapiObject>(NdapiConstants.D2FP_OBJ_GRP_CHILD_REAL_OBJ); }
