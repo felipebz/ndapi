75eb8c175c564189e7013c6ba692a38157a5ca99
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2014-12-25 19:45:14 -0200

    Add Editor documentation (closes #10)

--- Ndapi/Editor.cs
+++ Ndapi/Editor.cs
@@ -3,9 +3,37 @@ using Ndapi.Enums;
 
 namespace Ndapi
 {
+    /// <summary>
+    /// Represents an editor object.
+    /// </summary>
     public class Editor : NdapiObject
     {
+        /// <summary>
+        /// Creates an editor.
+        /// </summary>
+        /// <param name="module">Editor owner.</param>
+        /// <param name="name">Editor name.</param>
         public Editor(FormModule module, string name)
+        {
+            Create(name, ObjectType.Editor, module);
+        }
+
+        /// <summary>
+        /// Creates an editor.
+        /// </summary>
+        /// <param name="group">Editor owner.</param>
+        /// <param name="name">Editor name.</param>
+        public Editor(ObjectGroup group, string name)
+        {
+            Create(name, ObjectType.Editor, group);
+        }
+
+        /// <summary>
+        /// Creates an editor.
+        /// </summary>
+        /// <param name="module">Editor owner.</param>
+        /// <param name="name">Editor name.</param>
+        public Editor(ObjectLibrary module, string name)
         {
             Create(name, ObjectType.Editor, module);
         }
@@ -13,121 +41,182 @@ namespace Ndapi
         internal Editor(ObjectSafeHandle handle) : base(handle)
         {
         }
-
+            
+        /// <summary>
+        /// Gets or sets the background color.
+        /// </summary>
         public string BackgroundColor
         {
             get { return GetStringProperty(NdapiConstants.D2FP_BACK_COLOR); }
             set { SetStringProperty(NdapiConstants.D2FP_BACK_COLOR, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the bottom title.
+        /// </summary>
         public string BottomTitle
         {
             get { return GetStringProperty(NdapiConstants.D2FP_BTM_TTL); }
             set { SetStringProperty(NdapiConstants.D2FP_BTM_TTL, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the comment.
+        /// </summary>
         public string Comment
         {
             get { return GetStringProperty(NdapiConstants.D2FP_COMMENT); }
             set { SetStringProperty(NdapiConstants.D2FP_COMMENT, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the pattern to be used for the fill region.
+        /// Patterns are rendered in the two colors specified by <see cref="BackgroundColor"/> and <see cref="ForegroundColor"/>.
+        /// </summary>
         public string FillPattern
         {
             get { return GetStringProperty(NdapiConstants.D2FP_FILL_PAT); }
             set { SetStringProperty(NdapiConstants.D2FP_FILL_PAT, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the font name.
+        /// </summary>
         public string FontName
         {
             get { return GetStringProperty(NdapiConstants.D2FP_FONT_NAM); }
             set { SetStringProperty(NdapiConstants.D2FP_FONT_NAM, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the font size.
+        /// </summary>
         public int FontSize
         {
             get { return GetNumberProperty(NdapiConstants.D2FP_FONT_SIZ); }
             set { SetNumberProperty(NdapiConstants.D2FP_FONT_SIZ, value); }
         }
 
-        public int FontSpacing
+        /// <summary>
+        /// Gets or sets the font spacing.
+        /// </summary>
+        public FontSpacing FontSpacing
         {
-            get { return GetNumberProperty(NdapiConstants.D2FP_FONT_SPCING); }
+            get { return GetNumberProperty<FontSpacing>(NdapiConstants.D2FP_FONT_SPCING); }
             set { SetNumberProperty(NdapiConstants.D2FP_FONT_SPCING, value); }
         }
 
-        public int FontStyle
+        /// <summary>
+        /// Gets or sets the font style.
+        /// </summary>
+        public FontStyle FontStyle
         {
-            get { return GetNumberProperty(NdapiConstants.D2FP_FONT_STY); }
+            get { return GetNumberProperty<FontStyle>(NdapiConstants.D2FP_FONT_STY); }
             set { SetNumberProperty(NdapiConstants.D2FP_FONT_STY, value); }
         }
 
-        public int FontWeight
+        /// <summary>
+        /// Gets or sets the font weight.
+        /// </summary>
+        public FontWeight FontWeight
         {
-            get { return GetNumberProperty(NdapiConstants.D2FP_FONT_WGHT); }
+            get { return GetNumberProperty<FontWeight>(NdapiConstants.D2FP_FONT_WGHT); }
             set { SetNumberProperty(NdapiConstants.D2FP_FONT_WGHT, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the foreground color.
+        /// </summary>
         public string ForegroundColor
         {
             get { return GetStringProperty(NdapiConstants.D2FP_FORE_COLOR); }
             set { SetStringProperty(NdapiConstants.D2FP_FORE_COLOR, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the height.
+        /// </summary>
         public int Height
         {
             get { return GetNumberProperty(NdapiConstants.D2FP_HEIGHT); }
             set { SetNumberProperty(NdapiConstants.D2FP_HEIGHT, value); }
         }
 
-        public bool ShowHorizonatalScrollbar
+        /// <summary>
+        /// Gets or sets whether runtime should show a horizontal scrollbar.
+        /// </summary>
+        public bool ShowHorizontalScrollbar
         {
             get { return GetBooleanProperty(NdapiConstants.D2FP_SHOW_HORZ_SCRLBR); }
             set { SetBooleanProperty(NdapiConstants.D2FP_SHOW_HORZ_SCRLBR, value); }
         }
 
+        /// <summary>
+        /// Gets or sets whether runtime should show a vertical scrollbar.
+        /// </summary>
         public bool ShowVerticalScrollBar
         {
             get { return GetBooleanProperty(NdapiConstants.D2FP_SHOW_VERT_SCRLBR); }
             set { SetBooleanProperty(NdapiConstants.D2FP_SHOW_VERT_SCRLBR, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
         public string Title
         {
             get { return GetStringProperty(NdapiConstants.D2FP_TITLE); }
             set { SetStringProperty(NdapiConstants.D2FP_TITLE, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the visual attribute name.
+        /// </summary>
         public string VisualAttributeName
         {
             get { return GetStringProperty(NdapiConstants.D2FP_VAT_NAM); }
             set { SetStringProperty(NdapiConstants.D2FP_VAT_NAM, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the white-on-black property.
+        /// </summary>
         public bool WhiteOnBlack
         {
             get { return GetBooleanProperty(NdapiConstants.D2FP_WHITE_ON_BLACK); }
             set { SetBooleanProperty(NdapiConstants.D2FP_WHITE_ON_BLACK, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the width.
+        /// </summary>
         public int Width
         {
             get { return GetNumberProperty(NdapiConstants.D2FP_WIDTH); }
             set { SetNumberProperty(NdapiConstants.D2FP_WIDTH, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the content's wrap style.
+        /// </summary>
         public WrapStyle WrapStyle
         {
             get { return GetNumberProperty<WrapStyle>(NdapiConstants.D2FP_WRAP_STY); }
             set { SetNumberProperty(NdapiConstants.D2FP_WRAP_STY, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the X position measured at the upper left corner.
+        /// </summary>
         public int XPosition
         {
             get { return GetNumberProperty(NdapiConstants.D2FP_X_POS); }
             set { SetNumberProperty(NdapiConstants.D2FP_X_POS, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the Y position measured at the upper left corner.
+        /// </summary>
         public int YPosition
         {
             get { return GetNumberProperty(NdapiConstants.D2FP_Y_POS); }
