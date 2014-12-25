fd2d2f63422bc41631a7ca14e69fc59208ab48ff
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2014-12-25 21:38:49 -0200

    Add documentation in some enums

--- Ndapi/Enums/PropertyState.cs
+++ Ndapi/Enums/PropertyState.cs
@@ -1,11 +1,33 @@
 ﻿namespace Ndapi.Enums
 {
+    /// <summary>
+    /// Property states.
+    /// </summary>
     public enum PropertyState
     {
+        /// <summary>
+        /// State unknown.
+        /// </summary>
         Unknown,
+
+        /// <summary>
+        /// The property has its default value.
+        /// </summary>
         Default,
+
+        /// <summary>
+        /// The property had the default value and was overriden.
+        /// </summary>
         OverriddenDefault,
+
+        /// <summary>
+        /// The property is inherited.
+        /// </summary>
         Inherited,
+
+        /// <summary>
+        /// The property had and inherited value and was overriden.
+        /// </summary>
         OverriddenInherited
     }
 }
