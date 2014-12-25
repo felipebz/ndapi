af80f237ab3532596f63d8953e8c9af9fc9ca15d
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2014-12-25 19:22:29 -0200

    Add Menu documentation (closes #22)

--- Ndapi/Menu.cs
+++ Ndapi/Menu.cs
@@ -4,9 +4,27 @@ using System.Collections.Generic;
 
 namespace Ndapi
 {
+    /// <summary>
+    /// Represents a menu object.
+    /// </summary>
     public class Menu : NdapiObject
     {
+        /// <summary>
+        /// Creates a new menu.
+        /// </summary>
+        /// <param name="module">Menu owner.</param>
+        /// <param name="name">Menu name</param>
         public Menu(FormModule module, string name)
+        {
+            Create(name, ObjectType.Menu, module);
+        }
+
+        /// <summary>
+        /// Creates a new menu.
+        /// </summary>
+        /// <param name="module">Menu owner.</param>
+        /// <param name="name">Menu name</param>
+        public Menu(MenuModule module, string name)
         {
             Create(name, ObjectType.Menu, module);
         }
@@ -15,24 +33,36 @@ namespace Ndapi
         {
         }
 
+        /// <summary>
+        /// Gets or sets the comment.
+        /// </summary>
         public string Comment
         {
             get { return GetStringProperty(NdapiConstants.D2FP_COMMENT); }
             set { SetStringProperty(NdapiConstants.D2FP_COMMENT, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the subtitle.
+        /// </summary>
         public string Subtitle
         {
             get { return GetStringProperty(NdapiConstants.D2FP_SUB_TTL); }
             set { SetStringProperty(NdapiConstants.D2FP_SUB_TTL, value); }
         }
 
+        /// <summary>
+        /// Gets or sets whether the menu is a tear-off menu.
+        /// </summary>
         public bool TearOffMenu
         {
             get { return GetBooleanProperty(NdapiConstants.D2FP_TEAR_OFF_MNU); }
             set { SetBooleanProperty(NdapiConstants.D2FP_TEAR_OFF_MNU, value); }
         }
 
+        /// <summary>
+        /// Gets the menu items.
+        /// </summary>
         public IEnumerable<MenuItem> MenuItems => GetObjectList<MenuItem>(NdapiConstants.D2FP_MNU_ITM);
     }
 }
