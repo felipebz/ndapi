70abf69f467a8ae6697ddf16af7e5292d9fa9d97
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2014-12-25 20:40:49 -0200

    Add TabPage documentation (closes #38)

--- Ndapi/TabPage.cs
+++ Ndapi/TabPage.cs
@@ -4,41 +4,64 @@ using System.Collections.Generic;
 
 namespace Ndapi
 {
+    /// <summary>
+    /// Represents a tab page.
+    /// </summary>
     public class TabPage : NdapiObject
     {
-        public TabPage(Canvas module, string name)
+        /// <summary>
+        /// Creates a tab page.
+        /// </summary>
+        /// <param name="canvas">Canvas object.</param>
+        /// <param name="name">Tab page name.</param>
+        public TabPage(Canvas canvas, string name)
         {
-            Create(name, ObjectType.TabPage, module);
+            Create(name, ObjectType.TabPage, canvas);
         }
 
         internal TabPage(ObjectSafeHandle handle) : base(handle)
         {
         }
 
+        /// <summary>
+        /// Gets or sets the comment.
+        /// </summary>
         public string Comment
         {
             get { return GetStringProperty(NdapiConstants.D2FP_COMMENT); }
             set { SetStringProperty(NdapiConstants.D2FP_COMMENT, value); }
         }
 
+        /// <summary>
+        /// Gets or sets whether the tab is enabled.
+        /// </summary>
         public bool Enabled
         {
             get { return GetBooleanProperty(NdapiConstants.D2FP_ENABLED); }
             set { SetBooleanProperty(NdapiConstants.D2FP_ENABLED, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the label.
+        /// </summary>
         public string Label
         {
             get { return GetStringProperty(NdapiConstants.D2FP_LABEL); }
             set { SetStringProperty(NdapiConstants.D2FP_LABEL, value); }
         }
 
+        /// <summary>
+        /// Gets or sets whether the tab is visible.
+        /// </summary>
         public bool Visible
         {
             get { return GetBooleanProperty(NdapiConstants.D2FP_VISIBLE); }
             set { SetBooleanProperty(NdapiConstants.D2FP_VISIBLE, value); }
         }
 
+        /// <summary>
+        /// Gets all the graphic objects attached to the canvas.
+        /// </summary>
         public IEnumerable<Graphic> Graphics => GetObjectList<Graphic>(NdapiConstants.D2FP_GRAPHIC);
     }
 }
