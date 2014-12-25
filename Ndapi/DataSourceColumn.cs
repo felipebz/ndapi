60e7c0a78b138646ad7a81b716ea24258b9cee8a
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2014-12-25 18:31:06 -0200

    Add documentation for data source columns (closes #9)

--- Ndapi/DataSourceColumn.cs
+++ Ndapi/DataSourceColumn.cs
@@ -3,53 +3,82 @@ using Ndapi.Enums;
 
 namespace Ndapi
 {
+    /// <summary>
+    /// Represents a data source column.
+    /// </summary>
     public class DataSourceColumn : NdapiObject
     {
-        public DataSourceColumn(Block module, string name)
+        /// <summary>
+        /// Creates a data source column.
+        /// </summary>
+        /// <param name="block">Block object.</param>
+        /// <param name="name">Data source column name.</param>
+        public DataSourceColumn(Block block, string name)
         {
-            Create(name, ObjectType.DataSourceColumn, module);
+            Create(name, ObjectType.DataSourceColumn, block);
         }
 
         internal DataSourceColumn(ObjectSafeHandle handle) : base(handle)
         {
         }
 
+        /// <summary>
+        /// Gets or sets the data source column length.
+        /// </summary>
         public int Length
         {
             get { return GetNumberProperty(NdapiConstants.D2FP_DSC_LEN); }
             set { SetNumberProperty(NdapiConstants.D2FP_DSC_LEN, value); }
         }
 
+        /// <summary>
+        /// Gets or sets whether the data source column is mandatory.
+        /// </summary>
         public bool Mandatory
         {
             get { return GetBooleanProperty(NdapiConstants.D2FP_DSC_MANDATORY); }
             set { SetBooleanProperty(NdapiConstants.D2FP_DSC_MANDATORY, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the data source column name.
+        /// </summary>
         public override string Name
         {
             get { return GetStringProperty(NdapiConstants.D2FP_DSC_NAM); }
             set { SetStringProperty(NdapiConstants.D2FP_DSC_NAM, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the data source column precision.
+        /// </summary>
         public int Precision
         {
             get { return GetNumberProperty(NdapiConstants.D2FP_DSC_PRECISION); }
             set { SetNumberProperty(NdapiConstants.D2FP_DSC_PRECISION, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the data source column scale.
+        /// </summary>
         public int Scale
         {
             get { return GetNumberProperty(NdapiConstants.D2FP_DSC_SCALE); }
             set { SetNumberProperty(NdapiConstants.D2FP_DSC_SCALE, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the data source column type.
+        /// </summary>
         public DataSourceColumnType Type
         {
             get { return GetNumberProperty<DataSourceColumnType>(NdapiConstants.D2FP_DSC_TYP); }
             set { SetNumberProperty(NdapiConstants.D2FP_DSC_TYP, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the name of the data source column type.
+        /// </summary>
         public string TypeName
         {
             get { return GetStringProperty(NdapiConstants.D2FP_DSC_TYPE_NAME); }
