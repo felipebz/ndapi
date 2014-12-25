7bdd593b78430287736fa371363a8b7354fb68b0
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2014-12-25 18:35:53 -0200

    Add LibraryProgramUnit documentation (closes #19)

--- Ndapi/LibraryProgramUnit.cs
+++ Ndapi/LibraryProgramUnit.cs
@@ -3,14 +3,23 @@ using Ndapi.Enums;
 
 namespace Ndapi
 {
+    /// <summary>
+    /// Represents a library program unit.
+    /// </summary>
     public class LibraryProgramUnit : NdapiObject
     {
         internal LibraryProgramUnit(ObjectSafeHandle handle) : base(handle)
         {
         }
 
+        /// <summary>
+        /// Gets the program unit code.
+        /// </summary>
         public string Text => GetStringProperty(NdapiConstants.D2FP_PGU_TXT);
 
+        /// <summary>
+        /// Gets the program unit type.
+        /// </summary>
         public ProgramUnitType Type => GetNumberProperty<ProgramUnitType>(NdapiConstants.D2FP_PGU_TYP);
     }
 }
