ba240cadd3a4feed77336e6dd080d076288d9cf6
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2014-12-25 21:45:02 -0200

    Add LibraryModule documentation (closes #17)

--- Ndapi/LibraryModule.cs
+++ Ndapi/LibraryModule.cs
@@ -5,20 +5,40 @@ using System.Collections.Generic;
 
 namespace Ndapi
 {
+    /// <summary>
+    /// Represents a program unit library.
+    /// </summary>
     public class LibraryModule : NdapiModule
     {
         internal LibraryModule(ObjectSafeHandle handle) : base(handle)
         {
         }
 
+        /// <summary>
+        /// Gets all the attached libraries.
+        /// </summary>
         public IEnumerable<AttachedLibrary> AttachedLibraries => GetObjectList<AttachedLibrary>(NdapiConstants.D2FP_ATT_LIB);
 
+        /// <summary>
+        /// Gets the library location.
+        /// </summary>
         public string Location => GetStringProperty(NdapiConstants.D2FP_LIB_LOC);
 
+        /// <summary>
+        /// Gets all the program units.
+        /// </summary>
         public IEnumerable<LibraryProgramUnit> ProgramUnits => GetObjectList<LibraryProgramUnit>(NdapiConstants.D2FP_LIB_PROG_UNIT);
 
+        /// <summary>
+        /// Gets the source type.
+        /// </summary>
         public SourceType SourceType => GetNumberProperty<SourceType>(NdapiConstants.D2FP_LIB_SRC);
 
+        /// <summary>
+        /// Load the program unit library into memory.
+        /// </summary>
+        /// <param name="filename">Library location (.pll file)</param>
+        /// <returns>Loaded library reference.</returns>
         public new static LibraryModule Open(string filename)
         {
             ObjectSafeHandle library;
@@ -29,16 +49,25 @@ namespace Ndapi
             return new LibraryModule(library);
         }
 
+        /// <summary>
+        /// Program unit libraries does not support compilation.
+        /// </summary>
         public override void CompileFile()
         {
             throw new NdapiException("Library module does not support compilation");
         }
 
+        /// <summary>
+        /// Program unit libraries does not support compilation.
+        /// </summary>
         public override void CompileObjects()
         {
             throw new NdapiException("Library module does not support compilation");
         }
 
+        /// <summary>
+        /// Program unit libraries does not support saving.
+        /// </summary>
         public override void Save(string path = null, bool saveInDatabase = false)
         {
             throw new NdapiException("Library module does not support saving");
