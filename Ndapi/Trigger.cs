febcb065f37cbcbfcd662387cb878db75dc2675b
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2014-12-25 20:46:52 -0200

    Add trigger documentation (closes #39)

--- Ndapi/Trigger.cs
+++ Ndapi/Trigger.cs
@@ -4,59 +4,141 @@ using Ndapi.Enums;
 
 namespace Ndapi
 {
+    /// <summary>
+    /// Represents a trigger object.
+    /// </summary>
     public class Trigger : NdapiObject
     {
+        /// <summary>
+        /// Creates a trigger.
+        /// </summary>
+        /// <param name="module">Trigger owner.</param>
+        /// <param name="name">Trigger name.</param>
         public Trigger(FormModule module, string name)
         {
             Create(name, ObjectType.Trigger, module);
+        }
+
+        /// <summary>
+        /// Creates a trigger.
+        /// </summary>
+        /// <param name="group">Trigger owner.</param>
+        /// <param name="name">Trigger name.</param>
+        public Trigger(ObjectGroup group, string name)
+        {
+            Create(name, ObjectType.Trigger, group);
+        }
+
+        /// <summary>
+        /// Creates a trigger.
+        /// </summary>
+        /// <param name="library">Trigger owner.</param>
+        /// <param name="name">Trigger name.</param>
+        public Trigger(ObjectLibrary library, string name)
+        {
+            Create(name, ObjectType.Trigger, library);
+        }
+
+        /// <summary>
+        /// Creates a trigger.
+        /// </summary>
+        /// <param name="block">Trigger owner.</param>
+        /// <param name="name">Trigger name.</param>
+        public Trigger(Block block, string name)
+        {
+            Create(name, ObjectType.Trigger, block);
+        }
+
+        /// <summary>
+        /// Creates a trigger.
+        /// </summary>
+        /// <param name="item">Trigger owner.</param>
+        /// <param name="name">Trigger name.</param>
+        public Trigger(Item item, string name)
+        {
+            Create(name, ObjectType.Trigger, item);
+        }
+
+        /// <summary>
+        /// Creates a trigger.
+        /// </summary>
+        /// <param name="propertyClass">Trigger owner.</param>
+        /// <param name="name">Trigger name.</param>
+        public Trigger(PropertyClass propertyClass, string name)
+        {
+            Create(name, ObjectType.Trigger, propertyClass);
         }
 
         internal Trigger(ObjectSafeHandle handle) : base(handle)
         {
         }
 
+        /// <summary>
+        /// Gets or sets the comment.
+        /// </summary>
         public string Comment
         {
             get { return GetStringProperty(NdapiConstants.D2FP_COMMENT); }
             set { SetStringProperty(NdapiConstants.D2FP_COMMENT, value); }
         }
 
+        /// <summary>
+        /// Gets or sets whether runtime should display the trigger in keyboard help.
+        /// </summary>
         public bool DisplayInKeyboardHelp
         {
             get { return GetBooleanProperty(NdapiConstants.D2FP_DISP_IN_KBRD_HLP); }
             set { SetBooleanProperty(NdapiConstants.D2FP_DISP_IN_KBRD_HLP, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the execution style.
+        /// </summary>
         public ExecutionStyle ExecutionStyle
         {
             get { return GetNumberProperty<ExecutionStyle>(NdapiConstants.D2FP_EXEC_HIERARCHY); }
             set { SetNumberProperty(NdapiConstants.D2FP_EXEC_HIERARCHY, value); }
         }
 
+        /// <summary>
+        /// Gets or sets whether runtime should fire the trigger in enter-query mode.
+        /// </summary>
         public bool FireInEnterQueryMode
         {
             get { return GetBooleanProperty(NdapiConstants.D2FP_FIRE_IN_QRY); }
             set { SetBooleanProperty(NdapiConstants.D2FP_FIRE_IN_QRY, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the keyboard help description.
+        /// </summary>
         public string KeyboardHelpDescription
         {
             get { return GetStringProperty(NdapiConstants.D2FP_KBRD_HLP_TXT); }
             set { SetStringProperty(NdapiConstants.D2FP_KBRD_HLP_TXT, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the trigger style.
+        /// </summary>
         public TriggerStyle Style
         {
             get { return GetNumberProperty<TriggerStyle>(NdapiConstants.D2FP_TRG_STY); }
             set { SetNumberProperty(NdapiConstants.D2FP_TRG_STY, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the trigger code.
+        /// </summary>
         public string Text
         {
             get { return GetStringProperty(NdapiConstants.D2FP_TRG_TXT); }
             set { SetStringProperty(NdapiConstants.D2FP_TRG_TXT, value); }
         }
 
+        /// <summary>
+        /// Compiles the trigger.
+        /// </summary>
         public void Compile()
         {
             var status = NativeMethods.d2ftrgco_CompileObj(NdapiContext.Context, _handle);
