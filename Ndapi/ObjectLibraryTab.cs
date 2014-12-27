17ec3cfb4428aa76a0734cba36eae01ddb2ed846
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2014-12-27 13:13:08 -0200

    Add ObjectLibraryTab documentation (closes #28)

--- Ndapi/ObjectLibraryTab.cs
+++ Ndapi/ObjectLibraryTab.cs
@@ -1,38 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using Ndapi.Core.Handles;
 using Ndapi.Enums;
 using Ndapi.Core;
 
 namespace Ndapi
 {
+    /// <summary>
+    /// Represent an object library tab.
+    /// </summary>
     public class ObjectLibraryTab : NdapiObject
     {
-        public ObjectLibraryTab(FormModule module, string name)
+        /// <summary>
+        /// Creates an object library tab..
+        /// </summary>
+        /// <param name="library">Object library.</param>
+        /// <param name="name">Tab name.</param>
+        public ObjectLibraryTab(ObjectLibrary library, string name) : base(name, ObjectType.ObjectLibraryTab, library)
         {
-            Create(name, ObjectType.ObjectLibraryTab, module);
         }
 
         internal ObjectLibraryTab(ObjectSafeHandle handle) : base(handle)
         {
         }
 
+        /// <summary>
+        /// Gets or sets the comment.
+        /// </summary>
         public string Comment
         {
             get { return GetStringProperty(NdapiConstants.D2FP_COMMENT); }
             set { SetStringProperty(NdapiConstants.D2FP_COMMENT, value); }
         }
 
+        /// <summary>
+        /// Gets or sets the label.
+        /// </summary>
         public string Label
         {
             get { return GetStringProperty(NdapiConstants.D2FP_LABEL); }
             set { SetStringProperty(NdapiConstants.D2FP_LABEL, value); }
         }
 
+        /// <summary>
+        /// Gets the number of object in the tab.
+        /// </summary>
         public int ObjectCount => GetNumberProperty(NdapiConstants.D2FP_OBJ_COUNT);
 
+        /// <summary>
+        /// Gets all the objects contained in this tab.
+        /// </summary>
         public IEnumerable<NdapiObject> Objects
         {
             get
@@ -44,6 +60,11 @@ namespace Ndapi
             }
         }
 
+        /// <summary>
+        /// Get the object in the specified position.
+        /// </summary>
+        /// <param name="position">Position.</param>
+        /// <returns>The object found.</returns>
         public NdapiObject GetObjectByPosition(int position)
         {
             ObjectSafeHandle obj;
