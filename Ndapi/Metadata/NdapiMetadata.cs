d09896e250c2ae910b5f9dab8b9ec0c570d31b2c
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2015-01-05 21:47:01 -0200

    Add NdapiMetadata

---
+++ Ndapi/Metadata/NdapiMetadata.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ndapi.Metadata
+{
+    public static class NdapiMetadata
+    {
+        private static readonly Lazy<IEnumerable<Type>> _classes = new Lazy<IEnumerable<Type>>(LoadClasses);
+
+        public static IEnumerable<Type> Classes => _classes.Value;
+
+        private static IEnumerable<Type> LoadClasses()
+        {
+            return typeof(NdapiContext).Assembly.GetTypes().Where(t => t.BaseType == typeof(NdapiObject));
+        }
+    }
+}
