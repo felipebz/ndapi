78760937b8faf83fc936123d3d073751eace8b54
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2015-01-06 12:47:03 -0200

    Add missing file

---
+++ Ndapi/PropertyAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ndapi
+{
+    internal sealed class PropertyAttribute : Attribute
+    {
+        internal int PropertyId { get; }
+
+        public PropertyAttribute(int propertyId)
+        {
+            PropertyId = propertyId;
+        }
+    }
+}
