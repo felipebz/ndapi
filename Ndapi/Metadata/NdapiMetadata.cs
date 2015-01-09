d4bbff8f430b7c5cc3fe85734f0779cfb2c8c873
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2015-01-08 22:16:43 -0200

    Added NdapiMetadata.ObjectTypeMapping

--- Ndapi/Metadata/NdapiMetadata.cs
+++ Ndapi/Metadata/NdapiMetadata.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ndapi.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,18 +7,72 @@ namespace Ndapi.Metadata
 {
     public static class NdapiMetadata
     {
-        private static readonly Lazy<IEnumerable<Type>> _classes = new Lazy<IEnumerable<Type>>(LoadClasses);
+        private static readonly Lazy<Dictionary<ObjectType, Type>> _objectTypeMapping = new Lazy<Dictionary<ObjectType, Type>>(LoadObjectTypeMapping);
 
-        public static IEnumerable<Type> Classes => _classes.Value;
+        /// <summary>
+        /// Gets the Ndapi classes that represents a Forms API object.
+        /// </summary>
+        public static IEnumerable<Type> Classes => ObjectTypeMapping.Values;
 
-        private static IEnumerable<Type> LoadClasses()
+        /// <summary>
+        /// Gets a dictionary containing the relation bettwen an <see cref="ObjectType"/> and a Ndapi class.
+        /// </summary>
+        public static Dictionary<ObjectType, Type> ObjectTypeMapping => _objectTypeMapping.Value;
+
+        private static Dictionary<ObjectType, Type> LoadObjectTypeMapping()
         {
-            return typeof(NdapiContext).Assembly.GetTypes().Where(t => t.BaseType == typeof(NdapiObject));
+
+            return new Dictionary<ObjectType, Type>
+            {
+                { ObjectType.Alert,               typeof(Alert) },
+                { ObjectType.AttachedLibrary,     typeof(AttachedLibrary) },
+                { ObjectType.Block,               typeof(Block) },
+                { ObjectType.Canvas,              typeof(Canvas) },
+                //{ ObjectType.ColumnValue,       typeof(NdapiObject) },
+                //{ ObjectType.CompoundText,      typeof(NdapiObject) },
+                //{ ObjectType.Coordinate,        typeof(NdapiObject) },
+                { ObjectType.DataSourceArgument,  typeof(DataSourceArgument) },
+                { ObjectType.DataSourceColumn,    typeof(DataSourceColumn) },
+                { ObjectType.Editor,              typeof(Editor) },
+                //{ ObjectType.Font,              typeof(NdapiObject) },
+                { ObjectType.FormModule,          typeof(FormModule) },
+                { ObjectType.FormParameter,       typeof(FormParameter) },
+                { ObjectType.Graphics,            typeof(Graphics) },
+                { ObjectType.Item,                typeof(Item) },
+                { ObjectType.LibraryProgramUnit,  typeof(LibraryProgramUnit) },
+                { ObjectType.LibraryModule,       typeof(LibraryModule) },
+                { ObjectType.LOV,                 typeof(LOV) },
+                { ObjectType.ColumnMapping,       typeof(ColumnMapping) },
+                { ObjectType.Menu,                typeof(Menu) },
+                { ObjectType.MenuItem,            typeof(MenuItem) },
+                { ObjectType.MenuModule,          typeof(MenuModule) },
+                { ObjectType.MenuParameter,       typeof(MenuParameter) },
+                { ObjectType.ObjectGroup,         typeof(ObjectGroup) },
+                { ObjectType.ObjectGroupChild,    typeof(ObjectGroupChild) },
+                { ObjectType.ObjectLibrary,       typeof(ObjectLibrary) },
+                { ObjectType.ObjectLibraryTab,    typeof(ObjectLibraryTab) },
+                //{ ObjectType.Point,             typeof(NdapiObject) },
+                { ObjectType.ProgramUnit,         typeof(ProgramUnit) },
+                { ObjectType.PropertyClass,       typeof(PropertyClass) },
+                //{ ObjectType.RadioButton,       typeof(NdapiObject) },
+                { ObjectType.RecordGroup,         typeof(RecordGroup) },
+                { ObjectType.BlockRelation,       typeof(BlockRelation) },
+                { ObjectType.Report,              typeof(Report) },
+                { ObjectType.ColumnSpecification, typeof(RecordGroupColumn) },
+                { ObjectType.TabPage,             typeof(TabPage) },
+                //{ ObjectType.TextSegment,       typeof(NdapiObject) },
+                { ObjectType.Trigger,             typeof(Trigger) },
+                //{ ObjectType.TriggerStep,       typeof(NdapiObject) },
+                { ObjectType.VisualAttribute,     typeof(VisualAttribute) },
+                { ObjectType.Window,              typeof(Window) }
+            };
         }
 
-        public static NdapiMetaObject GetMetaObjectFrom(Type type)
-        {
-            return new NdapiMetaObject(type);
-        }
+        /// <summary>
+        /// Gets a <see cref="NdapiMetaObject"/> for the specified class.
+        /// </summary>
+        /// <param name="type">A Ndapi class.</param>
+        /// <returns>The meta object instance.</returns>
+        public static NdapiMetaObject GetMetaObjectFrom(Type type) => new NdapiMetaObject(type);
     }
 }
