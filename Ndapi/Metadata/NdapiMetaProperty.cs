1c71a59ab8418f288d26b26ec3314249c7a690e1
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2015-01-09 22:54:59 -0200

    Added NdapiMetaProperty.AllowedValues

--- Ndapi/Metadata/NdapiMetaProperty.cs
+++ Ndapi/Metadata/NdapiMetaProperty.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Ndapi.Metadata
 {
@@ -9,6 +10,7 @@ namespace Ndapi.Metadata
     public class NdapiMetaProperty
     {
         private static Dictionary<int, NdapiMetaProperty> _cache = new Dictionary<int, NdapiMetaProperty>();
+        private Lazy<Dictionary<int, string>> _allowedValues;
 
         /// <summary>
         /// Gets the property id;
@@ -40,6 +42,11 @@ namespace Ndapi.Metadata
         /// </summary>
         public string Description => Property.GetName(PropertyId);
 
+        /// <summary>
+        /// Gets the allowed values for the property
+        /// </summary>
+        public Dictionary<int, string> AllowedValues => _allowedValues.Value;
+
         private NdapiMetaProperty(int propertyId, string name, bool allowGet, bool allowSet, Type propertyType)
         {
             PropertyId = propertyId;
@@ -47,6 +54,7 @@ namespace Ndapi.Metadata
             AllowGet = allowGet;
             AllowSet = allowSet;
             PropertyType = propertyType;
+            _allowedValues = new Lazy<Dictionary<int, string>>(LoadAllowedValues);
         }
 
         internal static NdapiMetaProperty GetOrCreate(int propertyId, string name, bool allowGet, bool allowSet, Type propertyType)
@@ -60,6 +68,16 @@ namespace Ndapi.Metadata
             metaProperty = new NdapiMetaProperty(propertyId, name, allowGet, allowSet, propertyType);
             _cache.Add(propertyId, metaProperty);
             return metaProperty;
+        }
+
+        private Dictionary<int, string> LoadAllowedValues()
+        {
+            if (PropertyType.IsEnum)
+            {
+                return Enum.GetValues(PropertyType).Cast<int>().ToDictionary(e => e, e => Enum.GetName(PropertyType, e));
+            }
+
+            return new Dictionary<int, string>();
         }
 
         public override bool Equals(object obj)
