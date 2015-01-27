9251082d63e906b3011e0497c9df5f725befd8c8
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2015-01-26 22:21:12 -0200

    Remove TODO in ObjectSafeHandle

--- Ndapi/Core/Handles/ObjectSafeHandle.cs
+++ Ndapi/Core/Handles/ObjectSafeHandle.cs
@@ -4,9 +4,6 @@
     {
         protected override bool ReleaseHandleImpl()
         {
-            // TODO: try to figure out a way to do it.
-            //var status = NativeMethods.d2fobde_Destroy(NdapiContext.Context, handle);
-            //return status == (int)D2fErrorCode.D2FS_SUCCESS;
             return true;
         }
     }
