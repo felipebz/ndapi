ce7af13293098654fd329fe29ea25667b1f24455
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2023-12-26 09:53:35 -0300

    feat: Convert DllImport to LibraryImport

--- Ndapi/Core/D2fContext.cs
+++ Ndapi/Core/D2fContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
 
 namespace Ndapi.Core;
 
@@ -20,4 +21,42 @@ internal struct D2fContextAttributes
     public D2fMalloc d2fmalc_d2fctxa;
     public D2fFree d2fmfre_d2fctxa;
     public D2fRealloc d2fmrlc_d2fctxa;
+}
+
+[CustomMarshaller(typeof(D2fContextAttributes), MarshalMode.ManagedToUnmanagedRef, typeof(D2fContextAttributesMarshaler))]
+internal static unsafe class D2fContextAttributesMarshaler
+{
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct D2fContextAttributesUnmanaged
+    {
+        public int mask_d2fctxa;
+        public IntPtr cdata_d2fctxa;
+        public IntPtr d2fmalc_d2fctxa;
+        public IntPtr d2fmfre_d2fctxa;
+        public IntPtr d2fmrlc_d2fctxa;
+    }
+
+    internal static D2fContextAttributes ConvertToManaged(D2fContextAttributesUnmanaged unmanaged)
+    {
+        return new D2fContextAttributes
+        {
+            mask_d2fctxa = unmanaged.mask_d2fctxa,
+            cdata_d2fctxa = unmanaged.cdata_d2fctxa,
+            d2fmalc_d2fctxa = Marshal.GetDelegateForFunctionPointer<D2fMalloc>(unmanaged.d2fmalc_d2fctxa),
+            d2fmfre_d2fctxa = Marshal.GetDelegateForFunctionPointer<D2fFree>(unmanaged.d2fmfre_d2fctxa),
+            d2fmrlc_d2fctxa = Marshal.GetDelegateForFunctionPointer<D2fRealloc>(unmanaged.d2fmrlc_d2fctxa)
+        };
+    }
+
+    public static D2fContextAttributesUnmanaged ConvertToUnmanaged(D2fContextAttributes managed)
+    {
+        return new D2fContextAttributesUnmanaged
+        {
+            mask_d2fctxa = managed.mask_d2fctxa,
+            cdata_d2fctxa = managed.cdata_d2fctxa,
+            d2fmalc_d2fctxa = Marshal.GetFunctionPointerForDelegate(managed.d2fmalc_d2fctxa),
+            d2fmfre_d2fctxa = Marshal.GetFunctionPointerForDelegate(managed.d2fmfre_d2fctxa),
+            d2fmrlc_d2fctxa = Marshal.GetFunctionPointerForDelegate(managed.d2fmrlc_d2fctxa)
+        };
+    }
 }
