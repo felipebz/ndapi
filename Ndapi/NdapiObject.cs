76a5cf066e07f84b46cb221c5f6c6075bc76f44c
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2024-01-02 23:14:50 -0300

    style: Remove unused usings

--- Ndapi/NdapiObject.cs
+++ Ndapi/NdapiObject.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Text;
 
 using Ndapi.Core;
