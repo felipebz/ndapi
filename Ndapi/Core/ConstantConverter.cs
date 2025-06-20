a83c9704cda33419abbfb51cf68ac0cb3ba8ae40
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2025-06-19 21:23:35 -0300

    feat: Update ConstantConverter to handle Forms 14 properties

--- Ndapi/Core/ConstantConverter.cs
+++ Ndapi/Core/ConstantConverter.cs
@@ -6,29 +6,43 @@ internal static partial class ConstantConverter
 {
     public static int GetValue(Enum propertyId)
     {
-        var property = (NdapiConstant)propertyId;
-        return (NdapiContext.BuilderVersion.MajorVersion == 6) ? Forms6Constants[property] : Forms12Constants[property];
+        return GetValue((NdapiConstant)propertyId);
     }
 
     public static int GetValue(int propertyId)
     {
-        var property = (NdapiConstant)propertyId;
-        return (NdapiContext.BuilderVersion.MajorVersion == 6) ? Forms6Constants[property] : Forms12Constants[property];
+        return GetValue((NdapiConstant)propertyId);
+    }
+
+    private static int GetValue(NdapiConstant property)
+    {
+        return NdapiContext.BuilderVersion.MajorVersion switch
+        {
+            6 => Forms6Constants[property],
+            12 => Forms12Constants[property],
+            14 => Forms14Constants[property],
+            _ => throw new NdapiException($"Unsupported Forms Builder version ({NdapiContext.BuilderVersion.FullVersion})")
+        };
     }
 
     public static bool IsPropertySupportedByBuilderVersion(Enum propertyId)
     {
-        var property = (NdapiConstant)propertyId;
-        return (NdapiContext.BuilderVersion.MajorVersion == 6)
-            ? Forms6Constants.ContainsKey(property)
-            : Forms12Constants.ContainsKey(property);
+        return IsPropertySupportedByBuilderVersion((NdapiConstant)propertyId);
     }
 
     public static bool IsPropertySupportedByBuilderVersion(int propertyId)
     {
-        var property = (NdapiConstant)propertyId;
-        return (NdapiContext.BuilderVersion.MajorVersion == 6)
-            ? Forms6Constants.ContainsKey(property)
-            : Forms12Constants.ContainsKey(property);
+        return IsPropertySupportedByBuilderVersion((NdapiConstant)propertyId);
+    }
+
+    private static bool IsPropertySupportedByBuilderVersion(NdapiConstant property)
+    {
+        return NdapiContext.BuilderVersion.MajorVersion switch
+        {
+            6 => Forms6Constants.ContainsKey(property),
+            12 => Forms12Constants.ContainsKey(property),
+            14 => Forms12Constants.ContainsKey(property),
+            _ => throw new NdapiException($"Unsupported Forms Builder version ({NdapiContext.BuilderVersion.FullVersion})")
+        };
     }
 }
